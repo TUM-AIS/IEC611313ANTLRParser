bf3e984f7e58751694fbc52fee54c4a16977e386
Thomas Mikschl <mikschl@ais.mw.tum.de>
2022-10-11 11:28:44 +0200

    Initial commit

---
+++ IEC611313antlr/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("iec611313antlr")]
+[assembly: AssemblyDescription("Parser library for parsing IEC61131-3 ST and TIAOpeness SCL, STL code, using ANTLR4.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("iec611313antlr")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("94ce7ffe-fe9e-4261-8dac-29170ffd014c")]
+[assembly: AssemblyCompany("TU München / Lehrstuhl AIS")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyCopyright("Copyright © TU München / Lehrstuhl AIS 2022")]
+
